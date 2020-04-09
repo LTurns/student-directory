43e1b327cf521b0b823ca162d7c84a03f32e672f
LTurns <lizzieturney@gmail.com>
2020-04-09 12:00:38 +0100

    refactoring our methods

--- directory.rb
+++ directory.rb
@@ -1,37 +1,107 @@
+@students = [] #so the array is accessible to all methods @does not have to be in a method
+#to be accessible
+
 def input_students
   puts "Please enter the name of the students"
   puts "To finish, just hit return twice"
-  students = []
   name = gets.chomp
   #while the name is not empty, repeat this code
   while !name.empty? do
     #add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
  #add another name to students from the user
     name = gets.chomp
   end
   #return the array of input_students
-  students
+  @students
 end
 
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" # 9 because we'll be adding more items
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you mean, try again"
+  end
+end
 
 def print_header
   puts "The students of Villains Academy"
   puts "------------"
 end
 
-def print(students)
-  students.each do |student|
+def print_students_list
+  @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+interactive_menu
+
+
+
+
+
+
+
+
+
+
+
+
+=begin
+all of this code from one method can be divided into lots of smaller methods -
+refractoring as much as possible is always best.
+def interactive_menu
+  @students = []
+  loop do
+  # 1. print the menu and ask the user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit" # 9 because we'll be adding more items
+  # 2. read the input and save it into a variable
+    selection = gets.chomp
+  # 3. do what the user has asked
+  case selection
+  when "1"
+    students = input_students
+  when "2"
+    print_header
+    print(students)
+    print_footer(students)
+  when "9"
+    exit # this will cause the program to terminate
+  else
+    puts "I don't know what you meant, try again"
+  end
+  end
+end
+=end
